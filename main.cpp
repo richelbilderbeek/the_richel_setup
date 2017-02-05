23570ce94d0c457d3ffb8314114f2f1da69e7827
richelbilderbeek <richel@richelbilderbeek.nl>
2017-02-05 16:56:28 +0100

    Disallow profiling in debug mode

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "my_functions.h"
 #include <iostream>
 
-int main() {
+int main()
+{
+  #ifndef NDEBUG
+  #error(one should not profile on debug mode)
+  #endif
 
   std::cout << is_odd(42) << '\n';
   std::cout << calc_mean( { 41.0, 42.0, 43.0 } ) << '\n';
