a5a4c87ad64fb7fbd555803bd8dadae4c63882e3
richelbilderbeek <richel@richelbilderbeek.nl>
2017-02-08 20:38:32 +0100

    Do not profile deeper, prefer keeping code short instead

--- main.cpp
+++ main.cpp
@@ -6,9 +6,7 @@ int main()
   #ifndef NDEBUG
   #error(one should not profile on debug mode)
   #endif
-  for (int i=0; i!=1000; ++i)
-  {
-    std::cout << is_odd(42) << '\n';
-    std::cout << calc_mean( { 41.0, 42.0, 43.0 } ) << '\n';
-  }
+  
+  std::cout << is_odd(42) << '\n';
+  std::cout << calc_mean( { 41.0, 42.0, 43.0 } ) << '\n';
 }
